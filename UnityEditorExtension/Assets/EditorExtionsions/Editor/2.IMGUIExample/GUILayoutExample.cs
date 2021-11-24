f6ee21b3534dfaebbd4fe578213a1da297fa5dd0
summid <Summid@foxmail.com>
2021-11-24 23:58:00 +0800

    udpate IMGUI

--- UnityEditorExtension/Assets/EditorExtionsions/Editor/2.IMGUIExample/GUILayoutExample.cs
+++ UnityEditorExtension/Assets/EditorExtionsions/Editor/2.IMGUIExample/GUILayoutExample.cs
@@ -1,3 +1,5 @@
+using System.Runtime.InteropServices;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,6 +9,10 @@ namespace EditorExtensions
 {
     public class GUILayoutExample : EditorWindow
     {
+        private string textFieldValue;
+        private string textAreaValue;
+        private string passwordValue = String.Empty;//GUILayout.PasswordField 的参数需要有默认值，不能为空
+
         [MenuItem("EditorExtensions/02.IMGUI/01.GUILayoutExample")]
         static void OpenGUILayoutExample()
         {
@@ -15,7 +21,61 @@ namespace EditorExtensions
 
         private void OnGUI()
         {
-            GUILayout.Label("Hello IMGUI");
+            GUILayout.Label("Laybel: Hello IMGUI");
+
+            GUILayout.BeginVertical("box");//GUIStyle,参考GUI Skin文档
+            {
+                GUILayout.BeginHorizontal();
+                {
+                    GUILayout.Label("TextField");
+                    this.textFieldValue = GUILayout.TextField(this.textFieldValue);
+                }
+                GUILayout.EndHorizontal();
+
+                GUILayout.BeginHorizontal();
+                {
+                    GUILayout.Label("TextArea");
+                    this.textAreaValue = GUILayout.TextArea(this.textAreaValue);
+                }
+                GUILayout.EndHorizontal();
+
+                GUILayout.BeginHorizontal();
+                {
+                    GUILayout.Label("PasswordField");
+                    this.passwordValue = GUILayout.PasswordField(this.passwordValue, '*');
+                }
+                GUILayout.EndHorizontal();
+
+                GUILayout.BeginHorizontal();
+                {
+                    GUILayout.Label("Button");
+                    if (GUILayout.Button("Button"))
+                    {
+                        Debug.Log("Button Clicked.");
+                    }
+                }
+                GUILayout.EndHorizontal();
+
+                GUILayout.BeginHorizontal();
+                {
+                    GUILayout.Label("Repeat Button");
+                    if (GUILayout.RepeatButton("Repeat Button"))
+                    {
+                        Debug.Log("Repeat Button Clicked.");
+                    }
+                }
+                GUILayout.EndHorizontal();
+
+                GUILayout.BeginHorizontal();
+                {
+                    GUILayout.Label("Box");
+                    GUILayout.Box("Auto Layout Box");
+                }
+                GUILayout.EndHorizontal();
+
+            }
+            GUILayout.EndVertical();
+
         }
     }
 }
