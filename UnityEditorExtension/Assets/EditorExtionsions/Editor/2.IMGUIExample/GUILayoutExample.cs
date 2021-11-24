5ae371b017c8ea078a4a4df60fdd201124c9110c
summid <Summid@foxmail.com>
2021-11-24 23:21:14 +0800

    update IMGUI

--- UnityEditorExtension/Assets/EditorExtionsions/Editor/2.IMGUIExample/GUILayoutExample.cs
+++ UnityEditorExtension/Assets/EditorExtionsions/Editor/2.IMGUIExample/GUILayoutExample.cs
@@ -7,6 +7,15 @@ namespace EditorExtensions
 {
     public class GUILayoutExample : EditorWindow
     {
+        [MenuItem("EditorExtensions/02.IMGUI/01.GUILayoutExample")]
+        static void OpenGUILayoutExample()
+        {
+            GetWindow<GUILayoutExample>().Show();
+        }
 
+        private void OnGUI()
+        {
+            GUILayout.Label("Hello IMGUI");
+        }
     }
 }
