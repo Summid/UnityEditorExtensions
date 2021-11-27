f061bf35b54cfbbbd93053451296423e1aba10e5
summid <Summid@foxmail.com>
2021-11-27 21:06:27 +0800

    update all GUILayout API

--- UnityEditorExtension/Assets/EditorExtionsions/Editor/2.IMGUIExample/GUILayoutExample.cs
+++ UnityEditorExtension/Assets/EditorExtionsions/Editor/2.IMGUIExample/GUILayoutExample.cs
@@ -12,6 +12,11 @@ namespace EditorExtensions
         private string textFieldValue;
         private string textAreaValue;
         private string passwordValue = String.Empty;//GUILayout.PasswordField 的参数需要有默认值，不能为空
+        private Vector2 scrollPosition;
+        private float sliderValue;
+        private int toolbarIndex;
+        private bool toggleValue;
+        private int selectedGridIndex;
 
         [MenuItem("EditorExtensions/02.IMGUI/01.GUILayoutExample")]
         static void OpenGUILayoutExample()
@@ -23,58 +28,108 @@ namespace EditorExtensions
         {
             GUILayout.Label("Laybel: Hello IMGUI");
 
-            GUILayout.BeginVertical("box");//GUIStyle,参考GUI Skin文档
+            scrollPosition = GUILayout.BeginScrollView(scrollPosition);
             {
-                GUILayout.BeginHorizontal();
+                GUILayout.BeginVertical("box");//GUIStyle,参考GUI Skin文档
                 {
-                    GUILayout.Label("TextField");
-                    this.textFieldValue = GUILayout.TextField(this.textFieldValue);
-                }
-                GUILayout.EndHorizontal();
 
-                GUILayout.BeginHorizontal();
-                {
-                    GUILayout.Label("TextArea");
-                    this.textAreaValue = GUILayout.TextArea(this.textAreaValue);
-                }
-                GUILayout.EndHorizontal();
-
-                GUILayout.BeginHorizontal();
-                {
-                    GUILayout.Label("PasswordField");
-                    this.passwordValue = GUILayout.PasswordField(this.passwordValue, '*');
-                }
-                GUILayout.EndHorizontal();
-
-                GUILayout.BeginHorizontal();
-                {
-                    GUILayout.Label("Button");
-                    if (GUILayout.Button("Button"))
+                    GUILayout.BeginHorizontal();
                     {
-                        Debug.Log("Button Clicked.");
+                        GUILayout.Label("TextField");
+                        this.textFieldValue = GUILayout.TextField(this.textFieldValue);
                     }
-                }
-                GUILayout.EndHorizontal();
+                    GUILayout.EndHorizontal();
 
-                GUILayout.BeginHorizontal();
-                {
-                    GUILayout.Label("Repeat Button");
-                    if (GUILayout.RepeatButton("Repeat Button"))
+                    GUILayout.Space(50);//默认10px
+
+                    GUILayout.BeginHorizontal();
                     {
-                        Debug.Log("Repeat Button Clicked.");
+                        GUILayout.Label("TextArea");
+                        this.textAreaValue = GUILayout.TextArea(this.textAreaValue);
                     }
-                }
-                GUILayout.EndHorizontal();
+                    GUILayout.EndHorizontal();
 
-                GUILayout.BeginHorizontal();
-                {
-                    GUILayout.Label("Box");
-                    GUILayout.Box("Auto Layout Box");
-                }
-                GUILayout.EndHorizontal();
+                    GUILayout.BeginHorizontal();
+                    {
+                        GUILayout.Label("PasswordField");
+                        this.passwordValue = GUILayout.PasswordField(this.passwordValue, '*');
+                    }
+                    GUILayout.EndHorizontal();
 
+                    GUILayout.BeginHorizontal();
+                    {
+                        GUILayout.Label("Button");
+
+                        GUILayout.FlexibleSpace();//label和button分别为最左最右对其
+
+                        //同时设置按钮的最小、最大长宽
+                        if (GUILayout.Button("Button", GUILayout.MinWidth(100), GUILayout.MinHeight(100), GUILayout.MaxHeight(150), GUILayout.MaxWidth(150)))
+                        {
+                            Debug.Log("Button Clicked.");
+                        }
+                    }
+                    GUILayout.EndHorizontal();
+
+                    GUILayout.BeginHorizontal();
+                    {
+                        GUILayout.Label("Repeat Button");
+                        if (GUILayout.RepeatButton("Repeat Button", GUILayout.Width(150), GUILayout.Height(150)))//设置按钮宽高为150px
+                        {
+                            Debug.Log("Repeat Button Clicked.");
+                        }
+                    }
+                    GUILayout.EndHorizontal();
+
+                    GUILayout.BeginHorizontal();
+                    {
+                        GUILayout.Label("Box");
+                        GUILayout.Box("Auto Layout Box");
+                    }
+                    GUILayout.EndHorizontal();
+
+                    GUILayout.BeginHorizontal();
+                    {
+                        GUILayout.Label("Horizontal Slider");
+                        this.sliderValue = GUILayout.HorizontalSlider(this.sliderValue, 0, 1);
+                    }
+                    GUILayout.EndHorizontal();
+
+                    GUILayout.BeginHorizontal();
+                    {
+                        GUILayout.Label("Vertical Slider");
+                        this.sliderValue = GUILayout.VerticalSlider(this.sliderValue, 0, 1);
+                    }
+                    GUILayout.EndHorizontal();
+
+                    GUILayout.BeginArea(new Rect(0, 0, 100, 100));
+                    {
+                        GUI.Label(new Rect(0, 0, 20, 20), "Area");
+
+                    }
+                    GUILayout.EndArea();
+
+                    GUILayout.Window(1, new Rect(0, 0, 100, 100), id =>
+                    {
+
+                    }, "Window");
+
+                    this.toolbarIndex = GUILayout.Toolbar(this.toolbarIndex, new string[] { "ToolBar1", "ToolBar2", "ToolBar3", "ToolBar4", "ToolBar5" });
+
+                    this.toggleValue = GUILayout.Toggle(this.toggleValue, "Toggle");
+
+                    this.selectedGridIndex = GUILayout.SelectionGrid(selectedGridIndex, new string[]
+                    {
+                        "1",
+                        "2",
+                        "3",
+                        "4",
+                        "5"
+                    }, 2);
+
+                }
+                GUILayout.EndVertical();
             }
-            GUILayout.EndVertical();
+            GUILayout.EndScrollView();
 
         }
     }
