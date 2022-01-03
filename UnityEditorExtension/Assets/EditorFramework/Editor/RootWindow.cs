6d679a34e78ef4315fef2bda5a02ec7fb00badc6
summid <Summid@foxmail.com>
2022-01-03 14:57:17 +0800

    update RootWindow

--- UnityEditorExtension/Assets/EditorFramework/Editor/RootWindow.cs
+++ UnityEditorExtension/Assets/EditorFramework/Editor/RootWindow.cs
@@ -1,5 +1,6 @@
 using System.Reflection;
 using System;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
 using System.Linq;
@@ -9,6 +10,8 @@ namespace EditorFramework
 {
     public class RootWindow : EditorWindow
     {
+        private IEnumerable<Type> types;
+
         [MenuItem("EditorFramework/Open %#e")]
         static void Open()
         {
@@ -20,13 +23,24 @@ namespace EditorFramework
             Type editorWindowType = typeof(EditorWindow);
             // FieldInfo editorWindowParent = editorWindowType.GetField("m_Parent", BindingFlags.Instance | BindingFlags.NonPublic);//EditorWindow的host
 
-            var editorWindowTypes = AppDomain.CurrentDomain.GetAssemblies()//获取所有程序集
+            this.types = AppDomain.CurrentDomain.GetAssemblies()//获取所有程序集
                 .SelectMany(assembly => assembly.GetTypes())//将每个程序集中所有Type对象提取出来
                 .Where(type => type.IsSubclassOf(editorWindowType));//对Type对象进行筛选
+        }
 
-            foreach (var windowType in editorWindowTypes)
+        private void OnGUI()
+        {
+            foreach (var type in types)
             {
-                Debug.Log(windowType.Name);
+                GUILayout.BeginHorizontal("box");
+                {
+                    GUILayout.Label(type.Name);
+                    if (GUILayout.Button("Open", GUILayout.Width(80)))
+                    {
+                        GetWindow(type).Show();
+                    }
+                }
+                GUILayout.EndHorizontal();
             }
         }
 
