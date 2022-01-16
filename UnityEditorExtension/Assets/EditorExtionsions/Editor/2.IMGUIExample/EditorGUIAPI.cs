bd86144c2c18be80f6ff70da456d5e642d9df031
summid <Summid@foxmail.com>
2022-01-16 20:20:43 +0800

    update editorgui api

--- UnityEditorExtension/Assets/EditorExtionsions/Editor/2.IMGUIExample/EditorGUIAPI.cs
+++ UnityEditorExtension/Assets/EditorExtionsions/Editor/2.IMGUIExample/EditorGUIAPI.cs
@@ -15,7 +15,14 @@ namespace EditorExtensions
         private Rect toggleLeftRect = new Rect(10, 350, 100, 20);
         private Rect helpBoxRect = new Rect(10, 380, 200, 200);
         private Rect colorFieldRect = new Rect(10, 590, 100, 20);
-        private Rect boundsFieldRect = new Rect(10, 620, 500, 100);
+        private Rect boundsFieldRect = new Rect(10, 620, 500, 40);
+        private Rect boundsIntFiledRect = new Rect(10, 670, 500, 40);
+        private Rect curveFieldRect = new Rect(10, 720, 500, 40);
+        private Rect delayedDoubleFiledRect = new Rect(10, 770, 500, 20);
+        private Rect doubleFiledRect = new Rect(10, 800, 500, 20);
+        private Rect enumFlagesRect = new Rect(10, 830, 500, 20);
+        private Rect enumPopRect = new Rect(10, 860, 500, 20);
+        private Rect gradientRect = new Rect(10, 890, 500, 20);
 
         private bool disableGroupValue = false;
         private string textFieldValue;
@@ -24,32 +31,54 @@ namespace EditorExtensions
         private bool toggleValue;
         private Color colorFieldValue;
         private Bounds boundsFieldValue;
+        private BoundsInt boundsIntFieldValue;
+        private AnimationCurve curveFiledValue = new AnimationCurve();
+        private double doubleFiledValue;
+        private enum EnumFlagesValue { A = 1, B, C }
+        private EnumFlagesValue enumFlagesValue;
+        private bool foldOutValue = true;
+        private Gradient gradientValue = new Gradient();
 
         public void Draw()
         {
             this.disableGroupValue = EditorGUI.Toggle(this.toggleRect, new GUIContent("Disable Group"), this.disableGroupValue);
 
-            EditorGUI.BeginDisabledGroup(this.disableGroupValue);
+            this.foldOutValue = EditorGUI.Foldout(new Rect(300, 50, 100, 100), this.foldOutValue, "折叠");
+            if (this.foldOutValue)
             {
-                EditorGUI.LabelField(this.lableRect, "LabelField");
-                this.textFieldValue = EditorGUI.TextField(this.textFieldRect, this.textFieldValue);
-                this.textAreaValue = EditorGUI.TextArea(this.textAreaRect, this.textAreaValue);
-                this.passwordFieldValue = EditorGUI.PasswordField(this.passwordFieldRect, this.passwordFieldValue);
-                if (EditorGUI.DropdownButton(this.dropDownButtonRect, new GUIContent("123"), FocusType.Keyboard))
+                EditorGUI.BeginDisabledGroup(this.disableGroupValue);
                 {
-                    Debug.Log("DropdownButton Clicked");
+                    EditorGUI.LabelField(this.lableRect, "LabelField");
+                    this.textFieldValue = EditorGUI.TextField(this.textFieldRect, this.textFieldValue);
+                    this.textAreaValue = EditorGUI.TextArea(this.textAreaRect, this.textAreaValue);
+                    this.passwordFieldValue = EditorGUI.PasswordField(this.passwordFieldRect, this.passwordFieldValue);
+                    if (EditorGUI.DropdownButton(this.dropDownButtonRect, new GUIContent("123"), FocusType.Keyboard))
+                    {
+                        Debug.Log("DropdownButton Clicked");
+                    }
+
+                    this.toggleValue = EditorGUI.Toggle(this.toggleRightRect, this.toggleValue);
+                    this.toggleValue = EditorGUI.ToggleLeft(this.toggleLeftRect, "打开/关闭", this.toggleValue);
+
+                    EditorGUI.HelpBox(this.helpBoxRect, "123123123", MessageType.Error);
+
+                    this.colorFieldValue = EditorGUI.ColorField(this.colorFieldRect, this.colorFieldValue);
+
+                    this.boundsFieldValue = EditorGUI.BoundsField(this.boundsFieldRect, this.boundsFieldValue);
+                    this.boundsIntFieldValue = EditorGUI.BoundsIntField(this.boundsIntFiledRect, this.boundsIntFieldValue);
+
+                    this.curveFiledValue = EditorGUI.CurveField(this.curveFieldRect, this.curveFiledValue);
+
+                    this.doubleFiledValue = EditorGUI.DelayedDoubleField(this.delayedDoubleFiledRect, this.doubleFiledValue);
+                    this.doubleFiledValue = EditorGUI.DoubleField(this.doubleFiledRect, this.doubleFiledValue);
+
+                    this.enumFlagesValue = (EnumFlagesValue)EditorGUI.EnumFlagsField(this.enumFlagesRect, this.enumFlagesValue);
+                    this.enumFlagesValue = (EnumFlagesValue)EditorGUI.EnumPopup(this.enumPopRect, this.enumFlagesValue);
+
+                    this.gradientValue = EditorGUI.GradientField(this.gradientRect, this.gradientValue);
                 }
-
-                this.toggleValue = EditorGUI.Toggle(this.toggleRightRect, this.toggleValue);
-                this.toggleValue = EditorGUI.ToggleLeft(this.toggleLeftRect, "打开/关闭", this.toggleValue);
-
-                EditorGUI.HelpBox(this.helpBoxRect, "123123123", MessageType.Error);
-
-                this.colorFieldValue = EditorGUI.ColorField(this.colorFieldRect, this.colorFieldValue);
-
-                this.boundsFieldValue = EditorGUI.BoundsField(this.boundsFieldRect, this.boundsFieldValue);
+                EditorGUI.EndDisabledGroup();
             }
-            EditorGUI.EndDisabledGroup();
         }
     }
 
