4c836fcaff2d04a89ea1351d7fccdbb6450d10c6
summid <Summid@foxmail.com>
2022-02-15 20:36:30 +0800

    add MyAttributeExample

---
+++ UnityEditorExtension/Assets/EditorExtionsions/3.InpectorExample/3.AttributeExample/AttributeExample.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace EditorExtensions
+{
+    [HelpURL("http://unity.cn")]
+    public class AttributeExample : MonoBehaviour
+    {
+        public int Age;
+
+        [HideInInspector]
+        public int Age1;
+
+        [Header("ƒÍ¡‰")]
+        [SerializeField]
+        private int Age2;
+
+        [Space(20)]
+        public int Age3;
+
+        [Multiline(5)]
+        public string Comment;
+
+        [TextArea]
+        public string Comment1;
+
+        [Range(5,10)]
+        public int Age4;
+
+        [My]
+        public int Age5;
+    }
+}
