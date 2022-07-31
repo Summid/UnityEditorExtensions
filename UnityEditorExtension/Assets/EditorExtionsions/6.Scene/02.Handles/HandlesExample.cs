628a4af900e6e6f8ffd211109eab3cd336af8b6c
summid <Summid@foxmail.com>
2022-07-31 13:04:13 +0800

    add HandlesExample

---
+++ UnityEditorExtension/Assets/EditorExtionsions/6.Scene/02.Handles/HandlesExample.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace EditorExtensions
+{
+    public class HandlesExample : MonoBehaviour
+    {
+        /// <summary>
+        /// °ë¾¶
+        /// </summary>
+        public float Radius = 0.7f;
+    }
+}
