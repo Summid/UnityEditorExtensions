15c3e377fc1c9a5c47c0af93f3cfe7d2a8cf1c9f
summid <Summid@foxmail.com>
2022-08-10 20:14:09 +0800

    add EditorPrefsExample

---
+++ UnityEditorExtension/Assets/EditorExtionsions/7.Data/02.EditorPrefs/Editor/EditorPrefsExample.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+namespace EditorExtensions
+{
+    public class EditorPrefsExample
+    {
+        private const string key = "ThisIsAKey";
+
+        [MenuItem("EditorExtensions/04.Data/EditorPrefs/SaveTime")]
+        private static void SaveTime()
+        {
+            EditorPrefs.SetString(key, DateTime.Now.ToString());
+        }
+
+        [MenuItem("EditorExtensions/04.Data/EditorPrefs/ReadTime")]
+        private static void ReadTime()
+        {
+            Debug.Log(EditorPrefs.GetString(key));
+        }
+    }
+}
